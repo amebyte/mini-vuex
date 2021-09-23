49f32f0ad42e43cd9584010f00ef4d6069b3780f
unknown <416212316@qq.com>
2021-09-23 22:14:47 +0800

    念

---
+++ src/core/mini-vuex.js
@@ -0,0 +1,19 @@
+let Vue
+class Store{
+    constructor(options) {
+        this.$options = options
+    }
+}
+
+function install(_vue) {
+    Vue = _vue
+    Vue.mixin({
+        beforeCreate() {
+            if(this.$options.store) {
+                Vue.prototype.$store = this.$options.store
+            }
+        }
+    })
+}
+
+export default { Store, install }
